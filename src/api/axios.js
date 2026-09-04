934a9c6dafa3ae9117283251c7c9e669b90fd42d
Jospin Bahwere <jospinbahwere@gmail.com>
2026-09-04 15:19:31 +0200

    Connexion du frontend au backend Render

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
-import axios from "axios";
+﻿import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000"
+  baseURL: "https://jbk-microfinance-backend.onrender.com"
 });
 
 // Ajouter automatiquement le token JWT à chaque requête
